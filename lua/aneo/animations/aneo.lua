4535d7af75a72efd0cb29a6b7260a1b97bf7df31
Aman Babu Hemant <amanbabuhemant@gmail.com>
2025-05-11 02:13:10 +0530

    New Arts: aneo, illuminati, mini-yamato, mushrooms-in-a-jar

---
+++ lua/aneo/animations/aneo.lua
@@ -0,0 +1,32 @@
+--[[
+
+Mady by Aman Babu Hemant
+pixel by pixel
+
+--]]
+return {
+    title = "Aneo Logo",
+    name = "aneo",
+    width = 43,
+    height = 16,
+    frames = {
+        {
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", nil, nil, "66b75a", "66b75a", "66b75a", nil, "66b75a", "66b75a", "66b75a", "66b75a", nil, nil, nil, nil, "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", nil, nil, "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", nil, nil, },
+            { nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", nil, nil, nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "63ac3c", "63ac3c", "5c9b35", nil, nil, "66b75a", "63ac3c", "5c9b35", "5c9b35", "5c9b35", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "5c9b35", "5c9b35", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, nil, "007fcf", "007fcf", "007fcf", "007fcf", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "5c9b35", "007fcf", "007fcf", "007fcf", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", "007fcf", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", "007fcf", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, nil, nil, nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", "007fcf", nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "66b75a", "66b75a", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "5c9b35", "5c9b35", "5c9b35", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "5c9b35", "007fcf", "007fcf", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "63ac3c", "66b75a", "66b75a", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "66b75a", "66b75a", "66b75a", "66b75a", "66b75a", nil, nil, "66b75a", "63ac3c", "63ac3c", "66b75a", "66b75a", "66b75a", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, "66b75a", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "63ac3c", "5c9b35", "007fcf", nil, },
+            { nil, "66b75a", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "007fcf", nil, "66b75a", "5c9b35", "5c9b35", "007fcf", nil, nil, nil, "66b75a", "5c9b35", "5c9b35", "007fcf", nil, "66b75a", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "007fcf", nil, "66b75a", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "5c9b35", "007fcf", nil, },
+            { nil, nil, "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", nil, nil, "007fcf", "007fcf", "007fcf", nil, nil, nil, nil, "007fcf", "007fcf", "007fcf", nil, nil, "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", nil, nil, "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", "007fcf", nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+        }
+    }
+}
---
+++ lua/aneo/animations/illuminati.lua
@@ -0,0 +1,49 @@
+--[[
+
+Original Artwork: https://www.pixilart.com/art/illuminati-confirmed-e4f2c126bb3c746
+Artist Profile: https://www.pixilart.com/plafor
+Platform: PixilArt
+
+--]]
+return {
+    title = "Illuminati confirmed !",
+    name = "illuminati",
+    width = 32,
+    height = 32,
+    frames = {
+        {
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "4caf50", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "4caf50", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "4caf50", "2e7d32", "4caf50", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "2e7d32", "4caf50", "2e7d32", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "4caf50", "8bc34a", "4caf50", "4caf50", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "2e7d32", "2e7d32", "2e7d32", "2e7d32", "2e7d32", "2e7d32", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "2e7d32", "2e7d32", "2e7d32", "4caf50", "4caf50", "2e7d32", "2e7d32", "2e7d32", "2e7d32", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "2e7d32", "2e7d32", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "2e7d32", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "000000", nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "2e7d32", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "000000", nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "4caf50", "2e7d32", "8bc34a", "8bc34a", "ffffff", "ffffff", "ffffff", "4caf50", "4caf50", "4caf50", "4caf50", "8bc34a", "8bc34a", "4caf50", "000000", nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "4caf50", "2e7d32", "8bc34a", "000000", nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, "000000", "8bc34a", "8bc34a", "4caf50", "4caf50", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "8bc34a", "8bc34a", "2e7d32", "4caf50", "2e7d32", "8bc34a", "000000", nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, "000000", "4caf50", "4caf50", "000000", "000000", "000000", "4caf50", "ffffff", "ffffff", "ffffff", "4caf50", "000000", "000000", "000000", "8bc34a", "2e7d32", "8bc34a", "2e7d32", "000000", nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, "000000", "4caf50", "000000", "000000", "000000", "4caf50", "4caf50", "4caf50", "8bc34a", "ffffff", "8bc34a", "4caf50", "4caf50", "4caf50", "000000", "000000", "000000", "000000", "4caf50", "2e7d32", "000000", nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, "000000", "000000", "000000", "ffffff", "ffffff", "8bc34a", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "8bc34a", "ffffff", "ffffff", "000000", "000000", "4caf50", "4caf50", "000000", nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, "000000", "000000", "000000", "000000", "ffffff", "ffffff", "ffffff", "8bc34a", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "8bc34a", "ffffff", "ffffff", "ffffff", "8bc34a", "000000", "000000", "000000", "4caf50", "000000", nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "000000", "8bc34a", "ffffff", "ffffff", "ffffff", "8bc34a", "4caf50", "4caf50", "4caf50", "8bc34a", "ffffff", "ffffff", "ffffff", "ffffff", "8bc34a", "000000", "000000", "000000", "4caf50", "000000", nil, nil, nil, nil, },
+            { nil, nil, nil, nil, "000000", "4caf50", "4caf50", "8bc34a", "4caf50", "8bc34a", "8bc34a", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "ffffff", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "4caf50", "4caf50", "000000", nil, nil, nil, },
+            { nil, nil, nil, nil, "000000", "4caf50", "8bc34a", "8bc34a", "8bc34a", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "4caf50", "8bc34a", "8bc34a", "4caf50", "8bc34a", "4caf50", "000000", nil, nil, nil, },
+            { nil, nil, nil, "000000", "4caf50", "4caf50", "4caf50", "8bc34a", "4caf50", "8bc34a", "8bc34a", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "8bc34a", "8bc34a", "8bc34a", "4caf50", "8bc34a", "2e7d32", "4caf50", "2e7d32", "000000", nil, nil, },
+            { nil, nil, nil, "000000", "4caf50", "2e7d32", "8bc34a", "4caf50", "8bc34a", "4caf50", "8bc34a", "8bc34a", "8bc34a", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "4caf50", "8bc34a", "8bc34a", "8bc34a", "4caf50", "8bc34a", "4caf50", "4caf50", "2e7d32", "4caf50", "000000", nil, nil, },
+            { nil, nil, "000000", "2e7d32", "2e7d32", "8bc34a", "2e7d32", "2e7d32", "8bc34a", "2e7d32", "4caf50", "4caf50", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "8bc34a", "4caf50", "8bc34a", "4caf50", "4caf50", "2e7d32", "4caf50", "2e7d32", "4caf50", "2e7d32", "2e7d32", "000000", nil, },
+            { nil, nil, "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+            { nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, },
+        }
+    }
+}
